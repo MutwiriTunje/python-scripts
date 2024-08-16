45126ce9cbc9b40bc7c0a2e9658130bf1c7091a8
mutwiritunje <tunjegituma@gmail.com>
2024-08-16 17:04:28 +0300

    deleted incomplete and unused scripts

--- LESSON01/hello.py
+++
@@ -1,8 +0,0 @@
-weight = input("Weight: ")
-mass = input("(K)g or (L)bs: ")
-if mass.upper() == "K":
-    print("Weight in pounds: " + str(float(weight) * 2.2046226))
-elif mass.upper() == "L":
-    print("Weight in kilograms: " + str(float(weight)/2.2046226))
-else:
-    print("Wrong input!")
--- LESSON01/welcome.py
+++
@@ -1,8 +0,0 @@
-meaning = 42
-print('')
-
-if meaning > 10:
-    print('Right on')
-else:
-    print('Not today')
-    
